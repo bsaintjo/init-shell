88f0b4450e26f532718f58afc2db11199436c7bc
Brandon T. P. Saint-John <saint-john@lbl.gov>
2025-04-01 17:28:19 -0700

    Add error handling

--- clam/src/lib.rs
+++ clam/src/lib.rs
@@ -1,7 +1,9 @@
 #![no_std]
 use core::result::Result;
 
-use syscalls::{Errno, Sysno, syscall};
+use syscalls::{Sysno, syscall};
+
+pub use syscalls::Errno;
 
 pub mod logger;
 
@@ -9,9 +11,26 @@ const STDIN: usize = 0;
 const STDOUT: usize = 1;
 const STDERR: usize = 2;
 
-pub fn print<B: AsRef<[u8]> + ?Sized>(message: &B) -> Result<usize, Errno> {
+#[derive(Debug, thiserror::Error)]
+pub enum ShellError {
+    #[error("Syscall error")]
+    Errno(Errno),
+
+    #[error("Unknown command")]
+    UnknownCommand,
+}
+
+impl From<Errno> for ShellError {
+    fn from(errno: Errno) -> Self {
+        ShellError::Errno(errno)
+    }
+}
+
+pub fn print<B: AsRef<[u8]> + ?Sized>(message: &B) -> Result<usize, ShellError> {
     let message = message.as_ref();
-    unsafe { syscall!(Sysno::write, STDOUT, message.as_ptr(), message.len()) }
+    unsafe {
+        syscall!(Sysno::write, STDOUT, message.as_ptr(), message.len()).map_err(ShellError::from)
+    }
 }
 
 #[macro_export]
@@ -24,18 +43,22 @@ macro_rules! eprintln {
     };
 }
 
-pub fn eprint<B: AsRef<[u8]> + ?Sized>(message: &B) -> Result<usize, Errno> {
+pub fn eprint<B: AsRef<[u8]> + ?Sized>(message: &B) -> Result<usize, ShellError> {
     let message = message.as_ref();
-    unsafe { syscall!(Sysno::write, STDERR, message.as_ptr(), message.len()) }
+    unsafe {
+        syscall!(Sysno::write, STDERR, message.as_ptr(), message.len()).map_err(ShellError::from)
+    }
 }
 
-pub fn getcwd(buf: &mut [u8]) -> Result<usize, Errno> {
-    unsafe { syscall!(Sysno::getcwd, buf.as_mut_ptr(), buf.len()) }
+pub fn getcwd(buf: &mut [u8]) -> Result<usize, ShellError> {
+    unsafe { syscall!(Sysno::getcwd, buf.as_mut_ptr(), buf.len()).map_err(ShellError::from) }
 }
 
-pub fn read(buf: &mut [u8]) -> Result<usize, Errno> {
-    unsafe { syscall!(Sysno::read, STDIN, buf.as_mut_ptr(), buf.len()) }
+pub fn read(buf: &mut [u8]) -> Result<usize, ShellError> {
+    unsafe { syscall!(Sysno::read, STDIN, buf.as_mut_ptr(), buf.len()).map_err(ShellError::from) }
 }
-pub fn exit() -> Result<usize, Errno> {
-    unsafe { syscall!(Sysno::exit, 0) }
+
+pub fn exit() -> Result<(), Errno> {
+    let _ = unsafe { syscall!(Sysno::exit, 0) };
+    Ok(())
 }
